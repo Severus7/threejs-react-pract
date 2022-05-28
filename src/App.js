57580241cba26998ef098b184ddd5a7b76d0a4a5
Severus7 <joshuasapz@gmail.com>
2022-05-28 18:00:45 +0800

    added plane

--- src/App.js
+++ src/App.js
@@ -6,9 +6,18 @@ import "./App.css";
 
 function Box() {
   return (
-    <mesh>
+    <mesh position={[0, 2, 0]}>
       <boxBufferGeometry attach="geometry" />
       <meshLambertMaterial attach="material" color="hotpink" />
+    </mesh>
+  );
+}
+
+function Plane() {
+  return (
+    <mesh position={[0, 0, 0]} rotation={[-Math.PI / 2, 0, 0]}>
+      <planeBufferGeometry attach="geometry" args={[100, 100]} />
+      <meshLambertMaterial attach="material" color="lightblue" />
     </mesh>
   );
 }
@@ -21,6 +30,7 @@ function App() {
       <ambientLight intensity={0.5} />
       <spotLight position={[10, 15, 10]} angle={0.3} />
       <Box />
+      <Plane />
     </Canvas>
   );
 }
